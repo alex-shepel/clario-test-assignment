f574b01d1edbff73a30c95c6765c6fcf55e48ea0
Oleksandr Shepel <oleksandr.sh@mate.academy>
2025-01-28 13:18:42 +0200

    add base markup

---
+++ src/app/page.js
@@ -0,0 +1,39 @@
+'use client';
+import styles from './page.module.css';
+
+export default function Page() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form submitted!');
+  };
+
+  return (
+    <main className={styles.wrapper}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles.formGroup}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            required
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Create a password"
+            required
+          />
+        </div>
+        <button type="submit" className={styles.formSubmit}>
+          Sign Up
+        </button>
+      </form>
+    </main>
+  );
+}
