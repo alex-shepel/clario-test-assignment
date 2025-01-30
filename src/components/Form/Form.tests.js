531d688e6c3870425a819117a5f2a370b2fc9ac6
Oleksandr Shepel <oleksandr.sh@mate.academy>
2025-01-30 18:05:49 +0200

    simplify tests initialization

--- src/components/Form/Form.tests.js
+++ src/components/Form/Form.tests.js
@@ -1,29 +1,29 @@
-export const EMAIL_TESTS = new Map();
+export const EMAIL_TESTS = new Map([
+  ['regex', {
+    message: 'Invalid email address',
+    test: (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email),
+  }]
+]);
 
-EMAIL_TESTS.set('regex', {
-  message: 'Invalid email address',
-  test: (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email),
-});
-
-export const PASSWORD_TESTS = new Map();
-
-PASSWORD_TESTS.set('spaces', {
-  message: 'No spaces',
-  test: (password) => !/\s/.test(password),
-});
-PASSWORD_TESTS.set('minLength', {
-  message: '8 characters or more',
-  test: (password) => password.length >= 8,
-});
-PASSWORD_TESTS.set('maxLength', {
-  message: '64 characters or less',
-  test: (password) => password.length <= 64,
-});
-PASSWORD_TESTS.set('cases', {
-  message: 'Uppercase and lowercase letters',
-  test: (password) => /[A-Z]/.test(password) && /[a-z]/.test(password),
-});
-PASSWORD_TESTS.set('digits', {
-  message: 'At least one digit',
-  test: (password) => /\d/.test(password),
-});
+export const PASSWORD_TESTS = new Map([
+  ['spaces', {
+    message: 'No spaces',
+    test: (password) => !/\s/.test(password),
+  }],
+  ['minLength', {
+    message: '8 characters or more',
+    test: (password) => password.length >= 8,
+  }],
+  ['maxLength', {
+    message: '64 characters or less',
+    test: (password) => password.length <= 64,
+  }],
+  ['cases', {
+    message: 'Uppercase and lowercase letters',
+    test: (password) => /[A-Z]/.test(password) && /[a-z]/.test(password),
+  }],
+  ['digits', {
+    message: 'At least one digit',
+    test: (password) => /\d/.test(password),
+  }],
+]);
